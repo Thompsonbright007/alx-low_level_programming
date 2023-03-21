e483ded1ce3ea976487f74748e0163806e20ce0f
thompsonbright007 <brightthompson35@gmail.com>
2023-03-21 16:45:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+char a_z;
+for (a_z = 'a'; a_z <= 'z'; a_z++)
+{
+_putchar(a_z);
+}
+_putchar('\n');
+}
